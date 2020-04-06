0699376787fa74bcc73c11afcf949775903514d0
JadeMM <jade.moksness@gmail.com>
2020-04-06 10:40:21 -0700

    Updates

---
+++ precache-manifest.9dbc4fbdec01726a535a7bf4c39df33a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "eaf838a89cb5c9d3d42dfda6e4c3e12f",
+    "url": "/crime-rates-chart/index.html"
+  },
+  {
+    "revision": "8d13face34922bd39a9d",
+    "url": "/crime-rates-chart/static/css/main.535551c3.chunk.css"
+  },
+  {
+    "revision": "8d9126a65e2ae4c5e52d",
+    "url": "/crime-rates-chart/static/js/2.4b36ca23.chunk.js"
+  },
+  {
+    "revision": "7edc9d6a5411dd85ffa426b7d3cb6e20",
+    "url": "/crime-rates-chart/static/js/2.4b36ca23.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8d13face34922bd39a9d",
+    "url": "/crime-rates-chart/static/js/main.d082aad9.chunk.js"
+  },
+  {
+    "revision": "882baf933ac3bd1587f6",
+    "url": "/crime-rates-chart/static/js/runtime-main.6b92d425.js"
+  },
+  {
+    "revision": "0fa7f54e937927ec2dd5995bcaf8e14f",
+    "url": "/crime-rates-chart/static/media/data.0fa7f54e.csv"
+  }
+]);
